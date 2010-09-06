dc2428f5f432cf4246c4a7b4feaced1740c53b72
Davo Smith <git@davosmith.co.uk>
2010-09-06 21:58:53 +0100

    Changed required Moodle version, as this is now causing problems no upgrade

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
 $plugin->version = 2010031300;
-$plugin->requires = 2007020200;
+$plugin->requires = 2010090501;
 
 $submodule->version  = $plugin->version;
 $submodule->requires = $plugin->requires;
