1aca643f2e5669984f25253e7371b0bd19269e39
Davo Smith <git@davosmith.co.uk>
2011-05-07 09:51:34 +0100

    Fixed the 'view coments in new window' link (for teachers)

--- viewcomment.php
+++ viewcomment.php
@@ -1,11 +1,27 @@
 <?php
 
+// This file is part of the UploadPDF assignment type for Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 require_once(dirname(dirname(dirname(dirname(dirname(__FILE__))))).'/config.php');
 require_once(dirname(__FILE__).'/mypdflib.php');
 
 $id   = optional_param('id', 0, PARAM_INT);          // Course module ID
 $a    = optional_param('a', 0, PARAM_INT);           // Assignment ID
 $pageno = optional_param('pageno', 1, PARAM_INT);
+$userid = optional_param('userid', 0, PARAM_INT);
 
 $url = new moodle_url('/mod/assignment/type/uploadpdf/viewcomment.php', array('pageno'=>$pageno) );
 if ($id) {
@@ -35,12 +51,21 @@ if ($id) {
     $url->param('a', $a);
 }
 
+if ($userid) {
+    $url->param('userid', $userid);
+}
+
 $PAGE->set_url($url);
 require_login($course->id, false, $cm);
+
+if ($userid && $userid != $USER->id) {
+    require_capability('mod/assignment:grade', get_context_instance(CONTEXT_MODULE, $cm->id));
+} else {
+    require_capability('mod/assignment:submit', get_context_instance(CONTEXT_MODULE, $cm->id));
+    $userid = $USER->id;
+}
 
 require(dirname(__FILE__).'/assignment.class.php');
 $assignmentinstance = new assignment_uploadpdf($cm->id, $assignment, $cm, $course);
 
-$assignmentinstance->edit_comment_page($USER->id, $pageno, false);
-
-?>
+$assignmentinstance->edit_comment_page($userid, $pageno, false);
